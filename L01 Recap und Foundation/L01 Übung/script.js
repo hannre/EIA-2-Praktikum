54cefa770ce20c1887422e1cc31ba722b82086b8
hannre <48830963+hannre@users.noreply.github.com>
2019-10-06 22:33:17 +0200

    L01

---
+++ L01 Recap und Foundation/L01 Übung/script.js
@@ -0,0 +1,7 @@
+// L01 Recap und Foundation -- Übung
+let v = 1;
+v = v + 1;
+console.log(v);
+//let t: MapStringToBoolean = {"wert1" :true,"wert2": false};
+//console.log(t);
+//# sourceMappingURL=script.js.map
---
+++ L01 Recap und Foundation/L01 Übung/script.ts
@@ -0,0 +1,11 @@
+// L01 Recap und Foundation -- Übung
+
+let v:number = 1;
+v = v + 1;
+console.log(v);
+
+interface MapStringToBoolean {
+    [key: string]: boolean;
+    }
+//let t: MapStringToBoolean = {"wert1" :true,"wert2": false};
+//console.log(t);
